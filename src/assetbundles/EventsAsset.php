6a98757e308aba362e5d1e0d2040e63cf92306f8
Frederic Köberl <frederic.koeberl@gmail.com>
2019-10-14 15:28:01 +0200

    DEVSKY-29: Code formatting!

--- src/assetbundles/EventsAsset.php
+++ src/assetbundles/EventsAsset.php
@@ -1,15 +1,12 @@
 <?php
+
 namespace fredmansky\eventsky\assetbundles;
 
-use Craft;
 use craft\web\AssetBundle;
 use craft\web\assets\cp\CpAsset;
 
 class EventsAsset extends AssetBundle
 {
-    // Public Methods
-    // =========================================================================
-
     public function init()
     {
         $this->sourcePath = "@fredmansky/eventsky/resources/dist/";
