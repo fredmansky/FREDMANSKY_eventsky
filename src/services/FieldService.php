d796aedb33cb1e0f3eda9f600187a020cba9e059
Sarah Schütz <sarah@fredmansky.at>
2020-12-03 13:40:40 +0100

    craft 3.5 compatibility

--- src/services/FieldService.php
+++ src/services/FieldService.php
@@ -5,6 +5,7 @@ namespace fredmansky\eventsky\services;
 use Craft;
 use craft\base\Component;
 use craft\base\ElementInterface;
+use craft\events\ConfigEvent;
 use craft\helpers\ArrayHelper;
 use craft\services\Fields;
 use fredmansky\eventsky\elements\Event;
@@ -13,7 +14,12 @@ use fredmansky\eventsky\fields\EventTicketTypeMappingField;
 
 class FieldService extends Fields
 {
-    public function getFieldByHandle(string $handle)
+    /**
+     * @param string $handle
+     * @param null $context
+     * @return \craft\base\FieldInterface|mixed|null
+     */
+    public function getFieldByHandle(string $handle, $context = null)
     {
         return ArrayHelper::firstWhere($this->getAllFields(EventTicketTypeMappingField::FIELD_CONTEXT), 'handle', $handle, true);
     }
